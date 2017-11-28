4ae1e3df11ebb24bbcc99814d962a18534d3c700
Mike Wilcox <mwilcox56@gmail.com>
2017-11-28 10:22:59 -0500

    formatting

--- server/utils/store.js
+++ server/utils/store.js
@@ -1,8 +1,9 @@
 const redis = require('redis');
 const config = require('config');
+
 const redisClient = redis.createClient({
-    host: config.redis.host,
-    port: config.redis.port
+  host: config.redis.host,
+  port: config.redis.port
 });
 const store = require('alien-node-redis-utils')(redisClient);
 
