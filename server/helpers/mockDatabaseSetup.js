63bc047a524f945769ffe1735e88d5cce94bb820
Mike Wilcox <mwilcox56@gmail.com>
2017-12-01 14:47:15 -0500

    use redis config. wrap all promises.

--- server/helpers/mockDatabaseSetup.js
+++ server/helpers/mockDatabaseSetup.js
@@ -1,5 +1,9 @@
 const redis = require('redis');
-const redisClient = redis.createClient();
+const config = require('config');
+const redisClient = redis.createClient({
+  host: config.redis.host,
+  port: config.redis.port
+});
 const cacheUtils = require('alien-node-redis-utils')(redisClient);
 
 const conversation = require('../stubs/conversation.json');
@@ -19,42 +23,36 @@ const {
   ONE_WEEK_IN_MILLISECONDS
 } = require('../constants');
 
-cacheUtils.deleteItem(DB_CONVERSATIONS).then(() => {
-  cacheUtils
-    .setItem(DB_CONVERSATIONS, ONE_WEEK_IN_MILLISECONDS, conversation)
-    .then(process.exit);
-});
+let promises = [
+  cacheUtils.deleteItem(DB_CONVERSATIONS).then(() => {
+    cacheUtils.setItem(
+      DB_CONVERSATIONS,
+      ONE_WEEK_IN_MILLISECONDS,
+      conversation
+    );
+  }),
 
-cacheUtils.deleteItem(DB_COLLECTIONS).then(() => {
-  cacheUtils
-    .setItem(DB_COLLECTIONS, ONE_WEEK_IN_MILLISECONDS, collection)
-    .then(process.exit);
-});
+  cacheUtils.deleteItem(DB_COLLECTIONS).then(() => {
+    cacheUtils.setItem(DB_COLLECTIONS, ONE_WEEK_IN_MILLISECONDS, collection);
+  }),
 
-cacheUtils.deleteItem(DB_SERIES).then(() => {
-  cacheUtils
-    .setItem(DB_SERIES, ONE_WEEK_IN_MILLISECONDS, series)
-    .then(process.exit);
-});
+  cacheUtils.deleteItem(DB_SERIES).then(() => {
+    cacheUtils.setItem(DB_SERIES, ONE_WEEK_IN_MILLISECONDS, series);
+  }),
 
-cacheUtils.deleteItem(DB_MESSAGES).then(() => {
-  cacheUtils
-    .setItem(DB_MESSAGES, ONE_WEEK_IN_MILLISECONDS, messages)
-    .then(process.exit);
-});
+  cacheUtils.deleteItem(DB_MESSAGES).then(() => {
+    cacheUtils.setItem(DB_MESSAGES, ONE_WEEK_IN_MILLISECONDS, messages);
+  }),
 
-cacheUtils.deleteItem(DB_BLOCKS).then(() => {
-  cacheUtils
-    .setItem(DB_BLOCKS, ONE_WEEK_IN_MILLISECONDS, blocks)
-    .then(process.exit);
-});
+  cacheUtils.deleteItem(DB_BLOCKS).then(() => {
+    cacheUtils.setItem(DB_BLOCKS, ONE_WEEK_IN_MILLISECONDS, blocks);
+  }),
+  cacheUtils.deleteItem(DB_MEDIA).then(() => {
+    cacheUtils.setItem(DB_MEDIA, ONE_WEEK_IN_MILLISECONDS, media);
+  }),
+  cacheUtils.deleteItem(DB_TAG).then(() => {
+    cacheUtils.setItem(DB_TAG, ONE_WEEK_IN_MILLISECONDS, []);
+  })
+];
 
-cacheUtils.deleteItem(DB_MEDIA).then(() => {
-  cacheUtils
-    .setItem(DB_MEDIA, ONE_WEEK_IN_MILLISECONDS, media)
-    .then(process.exit);
-});
-
-cacheUtils.deleteItem(DB_TAG).then(() => {
-  cacheUtils.setItem(DB_TAG, ONE_WEEK_IN_MILLISECONDS, []).then(process.exit);
-});
+Promise.all(promises).then(process.exit);
