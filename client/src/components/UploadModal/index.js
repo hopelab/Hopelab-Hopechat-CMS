c5608f411afdbbdfdc540611679ed8f0b4db2925
Tim Garcia <tim.m.garcia@gmail.com>
2018-02-02 18:05:23 -0800

    Spinner for media upload

--- client/src/components/UploadModal/index.js
+++ client/src/components/UploadModal/index.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Modal} from 'reactstrap';
 import Dropzone from 'react-dropzone';
 import './style.css';
 
-const propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onHide: PropTypes.func.isRequired,
-  onUpload: PropTypes.func.isRequired,
+class UploadModal extends Component {
+  static propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+    onUpload: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false
+    };
+  }
+
+  render() {
+    const {
+      isOpen,
+      onHide,
+      onUpload,
+    } = this.props;
+
+    const { loading } = this.state;
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        toggle={() => {
+          this.setState({loading: false}, () => {
+            onHide();
+          })
+        }}
+        className='custom-modal d-flex'
+      >
+        <span>Drag and Drop Image Below To Upload</span>
+        <Dropzone
+          accept="image/jpeg, image/png, video/mp4, video/x-msvideo"
+          onDrop={(accepted, rejected) => {
+            this.setState({loading: true}, () => {
+              onUpload(accepted, rejected);
+            })
+          }}
+          className={`custom-dropzone`}
+        />
+        { loading ? <i className="fa fa-spinner fa-3x loading"></i> : undefined }
+      </Modal>
+    );
+  }
 }
 
-const UploadModal = ({
-  isOpen,
-  onHide,
-  onUpload,
-}) => (
-  <Modal
-    isOpen={isOpen}
-    toggle={onHide}
-    className='custom-modal'
-  >
-    <span>Drag and Drop Image Below To Upload</span>
-    <Dropzone
-      accept="image/jpeg, image/png, video/mp4, video/x-msvideo"
-      onDrop={onUpload}
-      className={`custom-dropzone`}
-    />
-  </Modal>
-);
-
-UploadModal.propTypes = propTypes;
 
 export default UploadModal;
